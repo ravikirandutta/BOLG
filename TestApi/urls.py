5850c553f78e8978fbb142db4611b477525ce443
sagardutta <f2003484@gmail.com>
2014-07-20 00:19:04 -0500

    checking in index.html file

--- TestApi/urls.py
+++ TestApi/urls.py
@@ -26,6 +26,6 @@ urlpatterns = patterns('',
     url(r'^takeaways/(?P<pk>[0-9]+)/$', TakeAwayDetail.as_view(),name='takeaway-detail'),
 	url(r'^takeaways/$', TakeAwayList.as_view(), name='takeaway-list'),
 	   url(r'^takeaways/test', 'takeaway.views.test', name='test'),
-
+       url(r'^takeaways/index', 'takeaway.views.index', name='index'),
     url(r'^takeaway/initload', 'takeaway.views.initload', name='initload'),
 )
