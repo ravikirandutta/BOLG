b381a99b840437e82607d0f696d4ea01fb38c770
ravikirandutta <ravi.dutta@gmail.com>
2014-07-20 10:55:42 -0500

    removed hyperlinking

--- takeaway/serializers.py
+++ takeaway/serializers.py
@@ -3,29 +3,29 @@ from rest_framework import serializers
 from takeaway.models import *
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'groups')
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
+class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ('url', 'name')
 
-class CourseSerializer(serializers.HyperlinkedModelSerializer):
+class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ('id', 'course_name', 'course_desc', 'students','session_set')
 
 
-class SessionSerializer(serializers.HyperlinkedModelSerializer):
+class SessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Session
         fields = ('id','session_name', 'session_dt','course','takeaway_set')
 		
-class TakeAwaySerializer(serializers.HyperlinkedModelSerializer):
+class TakeAwaySerializer(serializers.ModelSerializer):
     class Meta:
         model = TakeAway
         fields = ('id','notes', 'user','course','session','is_public')
