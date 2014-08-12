4543a8620f10ca1456b526afb8f3d996aab63187
rdutta <rdutta@rdutta-VirtualBox.(none)>
2014-08-12 01:54:19 -0500

    user in takeaway will be just the username and not the user pk

--- takeaway/serializers.py
+++ takeaway/serializers.py
@@ -6,8 +6,10 @@ from takeaway.models import *
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups')
-
+        #fields = ( 'username', 'email', 'groups')
+        fields = ('username')
+        
+    
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
@@ -20,13 +22,19 @@ class CourseSerializer(serializers.ModelSerializer):
         fields = ('id', 'course_name', 'course_desc', 'students','session_set')
 
 
+class TakeAwaySerializer(serializers.ModelSerializer):
+    user = serializers.Field(source='user.username')
+    class Meta:
+        model = TakeAway
+        fields = ('id','notes', 'user','course','session','is_public')
+        #depth = 1 
+
 class SessionSerializer(serializers.ModelSerializer):
+    takeaway_set = TakeAwaySerializer(source='takeaway_set')
+
     class Meta:
         model = Session
         fields = ('id','session_name', 'session_dt','course','takeaway_set')
         depth = 1
 
-class TakeAwaySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TakeAway
-        fields = ('id','notes', 'user','course','session','is_public')
+
