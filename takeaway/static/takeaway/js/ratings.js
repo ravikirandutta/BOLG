5c1b1dcfaf5b02786f24d2c95ffd8c0fa6f0400a
sagardutta <f2003484@gmail.com>
2014-08-22 23:42:50 -0500

    rating reflected and few css changes

--- takeaway/static/takeaway/js/ratings.js
+++ takeaway/static/takeaway/js/ratings.js
@@ -11,16 +11,23 @@
 		},
 		events : {"click":"rateTakeaway"},
 
-		rateTakeaway : function(){
+		rateTakeaway : function(event){
 			this.model;
 			var rating = new Rating();
 			var userId = this.model.get('user').id;
 			var takeAwayId = this.model.get('id');
-			var ratingValue = "5";
+			var ratingValue = event.target.value;
 			rating.set({'user':userId});
 
 			rating.set({'takeaway':takeAwayId});
 			rating.set({'rating_value':ratingValue});
 			rating.save();
+
+			var takeaway = this.model;
+			var average_rating = takeaway.get('average_rating');
+			var total_raters = takeaway.get('total_raters');
+			average_rating = ((average_rating*total_raters)+ ratingValue)/(total_raters+1);
+
+			takeaway.set({'average_rating':Number(average_rating).toFixed(1)});
 		}
 	});
