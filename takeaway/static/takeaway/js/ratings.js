8e99d3342c5fc2361ddc229383cf97399d7586ad
itzdarshureddy <itzdarshureddy@gmail.com>
2014-08-23 20:36:46 -0500

    rating changes

--- takeaway/static/takeaway/js/ratings.js
+++ takeaway/static/takeaway/js/ratings.js
@@ -5,8 +5,13 @@
 	var RatingsView = Backbone.View.extend({
 
 		render : function(){
+			if(!this.model.attributes.alreadyRated){
 			var template = _.template($('#rating-template').html(),this.model.toJSON());
 			$(this.el).html(template);
+		}
+		else{
+			$(this.el).html("| Your Rating("+this.model.get('rating')+")");
+		}
 			return this;
 		},
 		events : {"click":"rateTakeaway"},
@@ -24,11 +29,14 @@
 			rating.save();
 
 			var takeaway = this.model;
+			this.model.set({'alreadyRated':true});
+			this.model.set({'rating':ratingValue});
 			var average_rating = takeaway.get('average_rating');
 			var total_raters = takeaway.get('total_raters');
 			average_rating = ((Number(average_rating)*Number(total_raters))+ Number(ratingValue))/(Number(total_raters)+1);
 
 			takeaway.set({'average_rating':Number(average_rating).toFixed(1)});
+			this.render();
 		}
 	});
 
