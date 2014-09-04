7eb6a72a6e732b5bef2496116cad72e396a85d6f
sagardutta <f2003484@gmail.com>
2014-09-04 00:49:52 -0500

    logging to sysout so that papertrail picks it up

--- settings/live.py
+++ settings/live.py
@@ -1,5 +1,43 @@
+import sys
+
 DEBUG = False
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = ['www.mbatakeaways.com']
 
 AUTO_LOGOUT_DELAY = 5
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+       'console':{
+            'level':'INFO',
+            'class':'logging.StreamHandler',
+            'stream': sys.stdout
+        },,
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'INFO',
+        },
+        'takeaway': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
+
+
