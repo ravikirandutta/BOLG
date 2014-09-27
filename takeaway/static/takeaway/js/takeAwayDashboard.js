14469180d39a8390fec9d09f30ff07cf4f4854d0
svkreddy <svkreddymca@gmail.com>
2014-09-27 19:02:52 +0530

    Pending items added to file for reference
    Pending items added to file for reference

--- takeaway/static/takeaway/js/takeAwayDashboard.js
+++ takeaway/static/takeaway/js/takeAwayDashboard.js
@@ -1,3 +1,37 @@
+/* Comments Meta Data 
+    Course - CRS, Session - SN, Takeaway - TAY, Session and TakeAway - STAY
+*/
+
+/*
+  Pending:
+
+  1. Dialog box for editing session name
+  2. Dialog box for New Takeaway
+  3. Tags handling in New / Edit take away
+  4. PUB-PRI button in New / Edit take away
+  5. Selected CRS li tag class should be changed to "course-selected" and all others should be "course-deselected"
+  6. Owner check for enabling PUB-PRI button in each TAY
+  7. Owner check for enabling EDIT-DELTE buttons in each TAY
+  8. Favourite select/deselect and saving for each TAY
+  9. Confirmation dialog for DELETE TAY
+  10. Color difference for owned TAY
+  11. TAY style not rendering correctly
+  12. Expand and Collapse for sessions
+
+
+ng Modules required for below
+  Dialog window, Tags, RateIt and PUB-PRI button
+  
+
+  Open Questions:
+  1. Is Session can be deleted? If yes, all TAYs associated with that session also deleted?
+  2. Search required or not? If yes, on what basis? Tags/Rating/Owner/PUB-PRI/Favourite
+  3. Is user rated once, does he can't rate it again?
+  4. If user is owner for TAY, he can't rate it? But he can see it?
+  5. Is there any specific reason to code menu bar tags in this page?
+
+*/
+
 (function() {
   var app = angular.module('takeAwayDashboard', ['ngCookies', 'ngResource', 'ngRoute']).run(function($http, $cookies) {
     $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
@@ -42,6 +76,9 @@
         },
         save: {
           method: 'POST'
+        },
+        edit: {
+          method: 'PUT'
         }
       });
     }
@@ -61,7 +98,6 @@
     }
   ]);
 
-
   app.controller('takeawayDashboardCtrl', function($scope, $http, $cookies, $resource, CoursesFactory, SessionsFactory) {
 
     console.log("loading takeawayDashboardCtrl");
@@ -71,12 +107,13 @@
       "results": []
     };
 
+    // On Click of CRS, load all SNs and TAYs associated with the CRS.
     $scope.loadCourses = function(courseid) {
       SessionsFactory.query({
         "courseInstance": courseid,
         "ordering": "session_dt"
       }).$promise.then(function(data) {
-        console.log("inside course fetch then");
+        console.log("Load all SNs and TAYs associated with the CRS");
         $scope.sessions = data;
       });
       $scope.displaysessions = true;
@@ -114,8 +151,8 @@
           'Content-Type': 'application/json'
         }
       }).success(function(data, status, headers, config) {
-        taset = data;
-        alert("success");
+        //We need refresh only updated takeaway instead of loading all courses sessions
+        $scope.loadCourses(taset.courseInstance.id);
       }).error(function(data, status, headers, config) {
         $scope.status = status;
       });
@@ -127,6 +164,7 @@
       document.getElementById(divId + "_notes").value = "";
     };
 
+    //Deleting selected TAY by passing the ID
     $scope.deleteTakeaway = function(sessionsresult, taset) {
 
       //http://www.mbatakeaways.com/takeaways/45/
@@ -143,12 +181,19 @@
         $scope.status = status;
       });
     };
+
+
     CoursesFactory.query({
       "students": $cookies.userid
     }).$promise.then(function(data) {
-      console.log("inside course fetch then");
+      console.log("First load of the page load the all available CRS");
       $scope.availableCourses = data;
-    });
 
+      //Displaying the STAYs for first CRS
+      if($scope.availableCourses.results != null && $scope.availableCourses.results.length > 0) {
+        var defaultCourseId = $scope.availableCourses.results[0].course.id;  
+        $scope.loadCourses(defaultCourseId);
+      }
+    });
   });
 })();
