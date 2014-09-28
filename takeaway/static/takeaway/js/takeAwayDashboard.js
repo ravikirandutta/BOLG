656a79c331b019cac63a7a6c657ca9300a804177
svkreddy <svkreddymca@gmail.com>
2014-09-28 15:33:47 +0530

    New/Edit Takeaway worked.
    Done:
    Dialog box for editing session name
    Dialog box for New Takeaway
    Confirmation dialog for DELETE takeaway
    Progress:
    Tags, rate it, public/private button

--- takeaway/static/takeaway/js/takeAwayDashboard.js
+++ takeaway/static/takeaway/js/takeAwayDashboard.js
@@ -3,29 +3,26 @@
 */
 
 /*
-
-  
-
   Pending:
-
-  1. Dialog box for editing session name
-  2. Dialog box for New Takeaway
+  1. Dialog box for editing session name  -DONE
+  2. Dialog box for New Takeaway -DONE
   3. Tags handling in New / Edit take away
   4. PUB-PRI button in New / Edit take away
   5. Selected CRS li tag class should be changed to "course-selected" and all others should be "course-deselected"
   6. Owner check for enabling PUB-PRI button in each TAY
   7. Owner check for enabling EDIT-DELTE buttons in each TAY
   8. Favourite select/deselect and saving for each TAY
-  9. Confirmation dialog for DELETE TAY
+  9. Confirmation dialog for DELETE TAY -DONE
   10. Color difference for owned TAY
   11. TAY style not rendering correctly
   12. Expand and Collapse for sessions
+  13. Validations in Edit Session Name and New Takeaway and Edit Takeaway
+  14. On editing of takeaway, we should disable other takeaways edit-delete buttons and restore again after update/cancel
 
 
 ng Modules required for below
   Dialog window, Tags, RateIt and PUB-PRI button
   
-
   Open Questions:
   1. Is Session can be deleted? If yes, all TAYs associated with that session also deleted?
   2. Search required or not? If yes, on what basis? Tags/Rating/Owner/PUB-PRI/Favourite
@@ -68,7 +65,6 @@ ng Modules required for below
     });
   }]);
 
-  //http://www.mbatakeaways.com/courseInstances/?students=3
   //http://localhost:8000/sessions/?courseInstance=2&ordering=session_dt
 
   app.factory('CoursesFactory', ['$resource',
@@ -116,7 +112,7 @@ ng Modules required for below
     }
   ]);
 
-  app.controller('takeawayDashboardCtrl', function($scope, $http, $cookies, $resource, CoursesFactory, SessionsFactory, ngDialog) {
+  app.controller('takeawayDashboardCtrl', function($scope, $http, $cookies, $resource, $rootScope, CoursesFactory, SessionsFactory, ngDialog) {
 
     console.log("loading takeawayDashboardCtrl");
 
@@ -124,82 +120,6 @@ ng Modules required for below
     $scope.sessions = {
       "results": []
     };
-
-    // On Click of CRS, load all SNs and TAYs associated with the CRS.
-    $scope.loadCourses = function(courseid) {
-      SessionsFactory.query({
-        "courseInstance": courseid,
-        "ordering": "session_dt"
-      }).$promise.then(function(data) {
-        console.log("Load all SNs and TAYs associated with the CRS");
-        $scope.sessions = data;
-      });
-      $scope.displaysessions = true;
-    };
-
-
-    $scope.makeEditable = function(divId, notes) {
-      document.getElementById(divId + "_view").style.display = "none";
-      document.getElementById(divId + "_edit").style.display = "block";
-      document.getElementById(divId + "_notes").value = notes;
-    }
-
-
-    $scope.updateTakeaway = function(divId, taset) {
-      document.getElementById(divId + "_view").style.display = "block";
-      document.getElementById(divId + "_edit").style.display = "none";
-
-      //PUT: http://www.mbatakeaways.com/takeaways/45/
-      var temp = {
-        notes: document.getElementById(divId + "_notes").value,
-        user: $cookies.userid,
-        courseInstance: taset.courseInstance.id,
-        session: taset.session.id,
-        tags: [1],
-        is_public: taset.is_public
-      };
-
-      console.log(JSON.stringify(temp));
-
-      $http({
-        url: '/takeaways/' + taset.id,
-        method: "PUT",
-        data: temp,
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }).success(function(data, status, headers, config) {
-        //We need refresh only updated takeaway instead of loading all courses sessions
-        $scope.loadCourses(taset.courseInstance.id);
-      }).error(function(data, status, headers, config) {
-        $scope.status = status;
-      });
-    };
-
-    $scope.cancelEdit = function(divId) {
-      document.getElementById(divId + "_view").style.display = "block";
-      document.getElementById(divId + "_edit").style.display = "none";
-      document.getElementById(divId + "_notes").value = "";
-    };
-
-    //Deleting selected TAY by passing the ID
-    $scope.deleteTakeaway = function(sessionsresult, taset) {
-
-      //http://www.mbatakeaways.com/takeaways/45/
-
-      $http({
-        url: '/takeaways/' + taset.id,
-        method: "DELETE",
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }).success(function(data, status, headers, config) {
-        alert("deletion success");
-      }).error(function(data, status, headers, config) {
-        $scope.status = status;
-      });
-    };
-
 
     CoursesFactory.query({
       "students": $cookies.userid
@@ -214,6 +134,107 @@ ng Modules required for below
       }
     });
 
+    $scope.freshLoadOfSessions = function(courseid){
+      $scope.displaysessions = false;
+      $scope.loadCourses(courseid);
+    };
+
+    // On Click of CRS, load all SNs and TAYs associated with the CRS.
+    $scope.loadCourses = function(courseid) {
+      if($scope.displaysessions != true) {
+        SessionsFactory.query({
+          "courseInstance": courseid,
+          "ordering": "session_dt"
+        }).$promise.then(function(data) {
+          console.log("Load all SNs and TAYs associated with the CRS");
+          $scope.sessions = data;
+           //Below will localize the time instead of displaying it in UTC
+          _.each($scope.sessions.results,function(sessionsresult){
+            _.each(sessionsresult.takeaway_set,function(taset){
+              var localizedTime = $.localtime.toLocalTime(taset.created_dt,'MM/dd/yy HH:mm');
+              taset.created_dt =localizedTime;
+            });
+          });
+        });
+        $scope.displaysessions = true;
+      }
+    };
+
+
+    $scope.makeEditable = function(divId, notes) {
+      document.getElementById(divId + "_view").style.display = "none";
+      document.getElementById(divId + "_edit").style.display = "block";
+      document.getElementById(divId + "_notes").value = notes;
+    }
+
+    /* Update/Edit the takeaway and reload the page / refresh the takeaway part */
+    /* PUT: /takeaways/:takeawayID/ */
+    $scope.updateTakeaway = function(divId, taset) {
+      document.getElementById(divId + "_view").style.display = "block";
+      document.getElementById(divId + "_edit").style.display = "none";
+
+      var modifiedTakeawayObj = {
+        id : taset.id,
+        notes: document.getElementById(divId + "_notes").value,
+        user: $cookies.userid,
+        courseInstance: taset.courseInstance.id,
+        session: taset.session.id,
+        is_public: taset.is_public,
+        username : $cookies.username,
+        tags: [1],
+        created_dt : taset.created_dt,
+        average_rating : taset.average_rating,
+        total_raters : taset.total_raters
+      };
+
+      console.log(JSON.stringify(modifiedTakeawayObj));
+
+      $http({
+        url: '/takeaways/' + taset.id,
+        method: "PUT",
+        data: modifiedTakeawayObj,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).success(function(data, status, headers, config) {
+        //We need refresh only updated takeaway instead of loading all courses sessions
+        $scope.freshLoadOfSessions(taset.courseInstance.id);
+        console.log("loading all courses sessions after successfull edit");
+      }).error(function(data, status, headers, config) {
+        $scope.status = status;
+      });
+    };
+
+
+    $scope.cancelEdit = function(divId) {
+      document.getElementById(divId + "_view").style.display = "block";
+      document.getElementById(divId + "_edit").style.display = "none";
+      document.getElementById(divId + "_notes").value = "";
+    };
+
+    /*Deleting selected TAY by passing the ID */
+    /*TODO: confirmation dialog before delete */
+    /*DELETE: /takeaways/:id/ */
+    $scope.deleteTakeaway = function(sessionsresult, taset) {
+      var msg = "Are you sure you want to delete the take away notes?"
+      if(window.confirm(msg)) {
+        $http({
+        url: '/takeaways/' + taset.id,
+        method: "DELETE",
+        headers: {
+          'Content-Type': 'application/json'
+        }
+        }).success(function(data, status, headers, config) {
+          console.log('Takeaway Successfully deleted, now refresh the page');
+          $scope.freshLoadOfSessions(taset.courseInstance.id);
+        }).error(function(data, status, headers, config) {
+          $scope.status = status;
+          console.error('Error in deleteTakeaway'+status);
+        });
+      }
+    };
+
+    /* Displaying Dialog window to create New Take Away */
     $scope.newTakeaway = function (sessionsresult) {
       $scope.sessionsresult = sessionsresult;
       //$scope.takeaway_set = sessionsresult.takeaway_set[0];
@@ -221,37 +242,90 @@ ng Modules required for below
         template: 'newTakeawayTemplateId',
         controller: 'takeawayDashboardCtrl', 
         className: 'ngdialog-theme-plain',
-        scope: $scope,
-        data: {foo: 'some data'}
+        scope: $scope
       });
     };
 
-    $scope.sessionNameValue = "";
+    /* Event from "Save" button from New Take Away dialog window */
+    $scope.saveTakeaway = function(sessionsresult) {
 
+      $scope.newTakeawayObj = {
+        courseInstance: sessionsresult.courseInstance.id,
+        is_public: true,  //defalt value is true, should come from dialog window
+        notes: document.getElementById("notes").value,
+        session: sessionsresult.id,
+        tags: [1],
+        user: $cookies.userid
+      };
+
+      console.log('New Takeaway JSON: '+JSON.stringify($scope.newTakeawayObj));  
+
+      $http({
+        url: '/takeaways/',
+        method: "POST",
+        data: $scope.newTakeawayObj,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).success(function(data, status, headers, config) {
+        $scope.freshLoadOfSessions(data.courseInstance);
+        console.log("loading all courses sessions after successfull creation"+data.courseInstance);
+      }).error(function(data, status, headers, config) {
+        $scope.status = status;
+        console.error('Error in saveTakeaway'+status);
+      });
+
+      ngDialog.close();
+    };
+
+
+    /* Displaying Dialog window to update the session name */
     $scope.editSessionName = function (sessionsresult) {
+
+      $scope.session_name_current = sessionsresult.session_name;
+      $scope.courseInstanceId = sessionsresult.courseInstance.id;
+      $scope.modifiedSession = {
+        id:0,
+        session_name : "",
+        session_dt : ""
+      };
+
+      $scope.modifiedSession.id = sessionsresult.id;
+      $scope.modifiedSession.session_name = sessionsresult.session_name;
+      $scope.modifiedSession.session_dt = sessionsresult.session_dt;
+
       $scope.sessionsresult = sessionsresult;
-      //$scope.takeaway_set = sessionsresult.takeaway_set[0];
-      $scope.sessionNameValue = sessionsresult.session_name;
+
       ngDialog.open({
         template: 'editSessionNameTemplateId',
         controller: 'takeawayDashboardCtrl', 
         className: 'ngdialog-theme-plain',
-        scope: $scope,
-        data: {foo: 'some data'}
+        scope: $scope
       });
     };
 
-    $scope.saveTakeaway = function(sessionsresult) {
-      //console.log(JSON.stringify($scope.takeaway_set));
-      alert(document.getElementById("notes").value);
-      ngDialog.close();
-    };
-
-    $scope.updateSessionName = function(sessionsresult) {
-      //console.log(JSON.stringify($scope.takeaway_set));
-      alert(document.getElementById("editedSessionName").value);
+    /* Event from "Update" button from editSessionName dialog window */
+    /* PUT: /sessions/:sessionId */
+    $scope.updateSessionName = function(modifiedSession,sessionsresult) {
+      console.log(JSON.stringify(modifiedSession));
+      $http({
+          url: '/sessions/'+modifiedSession.id,
+          method: "PUT",
+          data: modifiedSession,
+          headers: {'Content-Type': 'application/json'}
+        }).success(function (data, status, headers, config) {
+          //$scope.freshLoadOfSessions($scope.courseInstanceId);
+          console.log("Successfully updated session name");
+          sessionsresult.session_name = modifiedSession.session_name;
+        }).error(function (data, status, headers, config) {
+          console.error('Error in updateSessionName'+status);
+      });
       ngDialog.close();
     };
       
+    /* Close the dialog for "New Takeaway" and "Edit Session Name" dialogs */
+    $scope.closeDialog = function() {
+      ngDialog.close();
+    }
   });
 })();
