ed7d3bf4c0ba990756bd948a1d4fbe2266f8f34e
rdutta <rdutta@rdutta-VirtualBox.(none)>
2014-10-19 01:36:12 -0500

    import os in models.py

--- takeaway/models.py
+++ takeaway/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django import forms
 from django.core.mail import send_mail,mail_admins
 from notifications import notify
+import os
 # Create your models here.
 
 import logging
